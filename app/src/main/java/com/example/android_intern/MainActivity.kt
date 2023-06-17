623af84caed31a2f98411f2a7cc82095c10439bb
Dezmonoid <Dezmoonoid@mail.ru>
2023-06-17 17:56:27 +0300

    Выполнение задания 4

--- app/src/main/java/com/example/android_intern/MainActivity.kt
+++ app/src/main/java/com/example/android_intern/MainActivity.kt
@@ -1,11 +1,20 @@
 package com.example.android_intern
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContentView(R.layout.activity_main)
+        val buttonOk: Button = findViewById(R.id.button_ok)
+        buttonOk.setOnClickListener { Toast.makeText(applicationContext,"Нажата ОК",Toast.LENGTH_SHORT).show()}
     }
+
+
 }
