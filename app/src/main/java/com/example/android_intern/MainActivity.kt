9d596cafe441ec349bfb086ce416fd847be48bbc
Dezmonoid <Dezmoonoid@mail.ru>
2023-06-17 18:41:57 +0300

    Выполнение задания 5

--- app/src/main/java/com/example/android_intern/MainActivity.kt
+++ app/src/main/java/com/example/android_intern/MainActivity.kt
@@ -2,10 +2,23 @@ package com.example.android_intern
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import timber.log.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        val TAG = "Intern"
+        Timber.plant(Timber.DebugTree())
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val buttonOk: Button = findViewById(R.id.button_ok)
+        val editText: EditText = findViewById(R.id.edit_text)
+        buttonOk.setOnClickListener {
+            Timber.tag(TAG).i(editText.text.toString())
+            Log.i("Task5", "projectInfo")
+        }
+
     }
 }
