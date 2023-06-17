173249ca69df7d868f8de9d75d1958f3fb32e1c9
Dezmonoid <Dezmoonoid@mail.ru>
2023-06-17 19:53:43 +0300

    Выполнение задания 8

--- app/src/main/java/com/example/android_intern/MainActivity.kt
+++ app/src/main/java/com/example/android_intern/MainActivity.kt
@@ -1,11 +1,20 @@
 package com.example.android_intern
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val buttonUse: Button = findViewById(R.id.button_listen)
+        val textView: TextView = findViewById(R.id.text_view)
+        buttonUse.setOnClickListener {
+            textView.text = "Нажата кнопка"
+        }
     }
 }
