fe6fad756e5436b6b1052f545f6ef557c6cdb3a3
Dezmonoid <Dezmoonoid@mail.ru>
2023-06-20 19:12:31 +0300

    Выполнение задания 9

--- app/src/main/java/com/example/android_intern/MainActivity.kt
+++ app/src/main/java/com/example/android_intern/MainActivity.kt
@@ -1,11 +1,29 @@
 package com.example.android_intern
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.CheckBox
+import android.widget.EditText
+import android.widget.ProgressBar
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val buttonOk: Button = findViewById(R.id.button)
+        val checkBox: CheckBox = findViewById(R.id.check_box)
+        val textView: TextView = findViewById(R.id.text_view)
+        val userText: EditText = findViewById(R.id.edit_text)
+        val loading: ProgressBar = findViewById(R.id.progress_bar)
+        buttonOk.setOnClickListener {
+            if (checkBox.isChecked) {
+                textView.text = userText.text
+                loading.progress += 10
+            }
+        }
     }
 }
