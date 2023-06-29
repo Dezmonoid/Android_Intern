afcc9f0a744f60d94e26b7ba9401187d5a7fec7f
Dezmonoid <Dezmoonoid@mail.ru>
2023-06-29 20:21:15 +0300

    Выполнение задания 12

--- app/src/main/java/com/example/android_intern/MainActivity.kt
+++ app/src/main/java/com/example/android_intern/MainActivity.kt
@@ -2,10 +2,22 @@ package com.example.android_intern
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import java.net.HttpURLConnection
+import java.net.URL
+import kotlin.concurrent.thread
 
+private const val URL="https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=ff49fcd4d4a08aa6aafb6ea3de826464&tags=cat&format=json&nojsoncallback=1"
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        thread {
+            val connection = URL(URL).openConnection() as HttpURLConnection
+            val readData = connection.inputStream.bufferedReader().readText()
+            Log.i("Task12",readData)
+
+        }
     }
 }
