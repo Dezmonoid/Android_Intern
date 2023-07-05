a5cb5863272614ca1c814d05be1def564883069d
Dezmonoid <Dezmoonoid@mail.ru>
2023-07-05 22:04:23 +0300

    Выполнение задания 13

--- app/src/main/java/com/example/android_intern/MainActivity.kt
+++ app/src/main/java/com/example/android_intern/MainActivity.kt
@@ -2,10 +2,34 @@ package com.example.android_intern
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import okhttp3.*
+import java.io.IOException
+
+private const val URL =
+    "https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=ff49fcd4d4a08aa6aafb6ea3de826464&tags=cat&format=json&nojsoncallback=1"
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val client = OkHttpClient()
+        val request = Request.Builder()
+            .url(URL)
+            .build()
+        client.newCall(request).enqueue(object : Callback {
+            override fun onFailure(call: Call, e: IOException) {
+                Log.i("Error", e.message.toString())
+            }
+
+            override fun onResponse(call: Call, response: Response) {
+                response.use {
+                    if (!response.isSuccessful) {
+                        Log.i("Error", "Запрос к серверу не был успешен")
+                    }
+                    Log.i("Successfully", response.body!!.string())
+                }
+            }
+        })
     }
 }
