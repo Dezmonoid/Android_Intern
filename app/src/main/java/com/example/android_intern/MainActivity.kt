37568bf99c24335f1a3abe4467f800a358281562
Dezmonoid <Dezmoonoid@mail.ru>
2023-07-06 21:06:35 +0300

    Masterlist

--- app/src/main/java/com/example/android_intern/MainActivity.kt
+++ app/src/main/java/com/example/android_intern/MainActivity.kt
@@ -2,15 +2,10 @@ package com.example.android_intern
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.android_intern.databinding.ActivityMainBinding
-
-private lateinit var binding: ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-        binding.button.setOnClickListener { binding.tv.text = "Добрейший вечерочек" }
+        setContentView(R.layout.activity_main)
     }
 }
