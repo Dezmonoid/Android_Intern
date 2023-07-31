23543c0bede0a2cac7b72d0a50ba29defa1f6066
Dezmonoid <Dezmoonoid@mail.ru>
2023-07-31 22:21:48 +0300

    Выполнение задания 21

--- app/src/main/java/com/example/android_intern/MainActivity.kt
+++ app/src/main/java/com/example/android_intern/MainActivity.kt
@@ -1,153 +1,46 @@
 package com.example.android_intern
 
 
-import android.annotation.SuppressLint
 import android.os.Bundle
-import android.text.Editable
-import android.text.TextWatcher
-import android.widget.EditText
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
-import androidx.recyclerview.widget.DefaultItemAnimator
-import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.core.widget.addTextChangedListener
 import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
+import com.example.android_intern.databinding.ActivityMainBinding
 import com.google.gson.Gson
 
-const val PhoneBookJson = """[{
-    "name": "(Приёмная)",
-    "phone": "+375 (2239) 7-17-80",
-    "type": ""
-  },
-  {
-    "name": "(Бухгалтерия)",
-    "phone": "+375 (2239) 7-17-64",
-    "type": ""
-  },
-  {
-    "name": "(Бухгалтерия)",
-    "phone": "+375 (2239) 7-18-08",
-    "type": ""
-  },
-  {
-    "name": "(Юридическое бюро)",
-    "phone": "+375 (2239) 7-17-63",
-    "type": ""
-  },
-  {
-    "name": "(Отдел правовой и кадровой работы)",
-    "phone": "+375 (2239) 7-17-93",
-    "type": ""
-  },
-  {
-    "name": "(Отдел материально-технического снабжения)",
-    "phone": "+375 (2239) 7-18-12",
-    "type": ""
-  },
-  {
-    "name": "",
-    "phone": "+375 44 712 36 26",
-    "type": "Сектор сбыта бумаги"
-  },
-  {
-    "name": "(Реализация на внутренний рынок)",
-    "phone": "+375 (2239) 7-17-79",
-    "type": "Сектор сбыта бумаги"
-  },
-  {
-    "name": "(Реализация на внешний рынок)",
-    "phone": "+375 (2239) 4-11-77",
-    "type": "Сектор сбыта бумаги"
-  },
-  {
-    "name": "(Реализация на внутренний рынок)",
-    "phone": "+375 (2239) 7-18-25",
-    "type": "Сектор сбыта бумаги"
-  },
-  {
-    "name": "",
-    "phone": "+375 44 580 09 70",
-    "type": "Сектор сбыта продукции деревообработки"
-  },
-  {
-    "name": "(Реализация продукции деревообработки)",
-    "phone": "+375 (2239) 7-18-42",
-    "type": "Сектор сбыта продукции деревообработки"
-  },
-  {
-    "name": "(Реализация продукции деревообработки)",
-    "phone": "+375 (2239) 3-64-71",
-    "type": "Сектор сбыта продукции деревообработки"
-  },
-  {
-    "name": "",
-    "phone": "+375 29 352 25 20",
-    "type": "Реализация домов, бань, беседок, клеёного бруса"
-  },
-  {
-    "name": "",
-    "phone": "+375 (2239) 7-18-43",
-    "type": "Реализация домов, бань, беседок, клеёного бруса"
-  },
-  {
-    "name": "(приемная)",
-    "phone": "+375 (2239) 7-17-80",
-    "type": "Факс"
-  },
-  {
-    "name": "(отдел сбыта)",
-    "phone": "+375 (2239) 7-17-79",
-    "type": "Факс"
-  },
-  {
-    "name": "(отдел материально-технического снабжения)",
-    "phone": "+375 (2239) 7-17-82",
-    "type": "Факс"
-  },
-  {
-    "name": "(служба главного энергетика)",
-    "phone": "+375 (2239) 7-18-06",
-    "type": "Факс"
-  }]"""
+
+private const val TAG = "Tag"
 
 class MainActivity : AppCompatActivity() {
-
-
+    private lateinit var binding: ActivityMainBinding
+    private val gson = Gson()
+    private val phoneAdapter = PhoneAdapter()
     override fun onCreate(savedInstanceState: Bundle?) {
-
-
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        val recyclerPhoneView: RecyclerView = findViewById(R.id.RV1)
-        val filterText: EditText = findViewById(R.id.ET1)
-        val gson = Gson()
-        val phoneBook =
-            gson.fromJson(PhoneBookJson, Array<PhoneBook>::class.java).asList()
-        val phoneAdapter = PhoneAdapter(phoneBook,this)
-        recyclerPhoneView.apply {
-            hasFixedSize()
-            layoutManager = LinearLayoutManager(this@MainActivity)
-            addItemDecoration(DividerItemDecoration(this@MainActivity, DividerItemDecoration.VERTICAL))
-            itemAnimator = DefaultItemAnimator()
-            adapter = phoneAdapter
-                    }
-
-        filterText.addTextChangedListener(object : TextWatcher {
-            override fun afterTextChanged(s: Editable) {
-                phoneAdapter.submitList(phoneBook.filter{ "${it.name} ${it.phone} ${it.type}".contains(filterText.text) })
-            }
-            override fun beforeTextChanged(
-                s: CharSequence, start: Int,
-                count: Int, after: Int
-            ) {
-            }
-            override fun onTextChanged(
-                s: CharSequence, start: Int,
-                before: Int, count: Int
-            ) {
-
-            }
-        })
-
-
+        Log.d(TAG, "onCreate")
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        initRecyclerView()
+        phoneAdapter.submitList(getInitNumbers())
+        setListeners()
     }
+
+    private fun setListeners() {
+        binding.etInputFilter.addTextChangedListener {
+            val filteredList =
+                getInitNumbers().filter { "${it.name} ${it.phone} ${it.type}".contains(binding.etInputFilter.text) }
+            phoneAdapter.submitList(filteredList)
+        }
+    }
+
+    private fun getInitNumbers() =
+        gson.fromJson(phoneBookJson, Array<PhoneBook>::class.java).asList()
+
+    private fun initRecyclerView() {
+        binding.recyclerView.layoutManager =
+            LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
+        binding.recyclerView.adapter = phoneAdapter
+    }
+
 }
--- app/src/main/java/com/example/android_intern/PhoneAdapter.kt
+++ app/src/main/java/com/example/android_intern/PhoneAdapter.kt
@@ -1,56 +1,44 @@
 package com.example.android_intern
 
-import android.content.Context
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
+import com.example.android_intern.databinding.PhoneItemBinding
 
 
-class PhoneAdapter(private val phoneBook: List<PhoneBook>, val context: MainActivity) :
+class PhoneAdapter :
     ListAdapter<PhoneBook, PhoneViewHolder>(UserItemDiffCallback()) {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PhoneViewHolder {
-        return PhoneViewHolder(
-            LayoutInflater.from(context).inflate(R.layout.phone_item, parent, false)
-        )
-    }
-
-    override fun getItemCount(): Int {
-        return when {
-            (phoneBook.size <= 0) -> 0
-            else -> phoneBook.size
-        }
+        val binding = PhoneItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return PhoneViewHolder(binding)
     }
 
     override fun onBindViewHolder(holder: PhoneViewHolder, position: Int) {
-        holder.bind(phoneBook[position], context)
+        holder.bind(getItem(position))
     }
+}
 
+private class UserItemDiffCallback : DiffUtil.ItemCallback<PhoneBook>() {
+    override fun areItemsTheSame(oldItem: PhoneBook, newItem: PhoneBook): Boolean =
+        oldItem.phone == newItem.phone
 
-
-    class UserItemDiffCallback : DiffUtil.ItemCallback<PhoneBook>() {
-        override fun areItemsTheSame(oldItem: PhoneBook, newItem: PhoneBook): Boolean =
-            oldItem == newItem
-
-        override fun areContentsTheSame(oldItem: PhoneBook, newItem: PhoneBook): Boolean =
-            oldItem == newItem
-
-    }
-
+    override fun areContentsTheSame(oldItem: PhoneBook, newItem: PhoneBook): Boolean =
+        oldItem == newItem
 
 }
 
-class PhoneViewHolder(view: View) : RecyclerView.ViewHolder(view) {
-    val nameTV = view.findViewById<TextView>(R.id.TV1)
-    val phoneTV = view.findViewById<TextView>(R.id.TV2)
-    val typeTV = view.findViewById<TextView>(R.id.TV3)
+class PhoneViewHolder(private val binding: PhoneItemBinding) :
+    RecyclerView.ViewHolder(binding.root) {
 
-    fun bind(phoneBook: PhoneBook, context: Context) {
-        nameTV.text = "Name: ${phoneBook.name}"
-        phoneTV.text = "Phone: ${phoneBook.phone}"
-        typeTV.text = "Тype: ${phoneBook.type}"
+
+    fun bind(phoneBook: PhoneBook) {
+        binding.TV1.text = "Name: ${phoneBook.name}"
+        binding.TV2.text = binding.root.context.getString(
+            R.string.phone,
+            phoneBook.phone
+        )   //"Phone: ${phoneBook.phone}"
+        binding.TV3.text = "Тype: ${phoneBook.type}"
     }
 }
