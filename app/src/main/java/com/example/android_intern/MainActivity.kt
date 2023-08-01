10b8a1b045c89de8d641fcd990620c62f653e9cb
Dezmonoid <Dezmoonoid@mail.ru>
2023-08-01 21:32:16 +0300

    Внесение изменений в задание 12

--- app/src/main/java/com/example/android_intern/MainActivity.kt
+++ app/src/main/java/com/example/android_intern/MainActivity.kt
@@ -7,17 +7,29 @@ import java.net.HttpURLConnection
 import java.net.URL
 import kotlin.concurrent.thread
 
-private const val URL="https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=ff49fcd4d4a08aa6aafb6ea3de826464&tags=cat&format=json&nojsoncallback=1"
-class MainActivity : AppCompatActivity() {
+private const val URL =
+    "https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=ff49fcd4d4a08aa6aafb6ea3de826464&tags=cat&format=json&nojsoncallback=1"
+private const val TAG = "Message"
 
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        thread {
-            val connection = URL(URL).openConnection() as HttpURLConnection
-            val readData = connection.inputStream.bufferedReader().readText()
-            Log.i("Task12",readData)
+        getMessageToLog()
+    }
 
+    private fun getMessageToLog() {
+        thread {
+            val urlConnection = URL(URL).openConnection() as HttpURLConnection
+            try {
+                val readData = urlConnection.inputStream.bufferedReader().readText()
+                Log.i(TAG, readData)
+            } catch (e: Exception) {
+                Log.e(TAG, e.stackTraceToString())
+            } finally {
+                urlConnection.disconnect();
+            }
         }
     }
+
 }
