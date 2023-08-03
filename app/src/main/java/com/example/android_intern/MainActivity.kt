8cda65420b6360336fea3354cd5157ee0c2a88c3
Dezmonoid <Dezmoonoid@mail.ru>
2023-08-03 21:05:24 +0300

    Выполнение задания 22

--- app/src/main/java/com/example/android_intern/MainActivity.kt
+++ app/src/main/java/com/example/android_intern/MainActivity.kt
@@ -1,8 +1,9 @@
 package com.example.android_intern
 
 
+import android.content.Context
+import android.content.SharedPreferences
 import android.os.Bundle
-import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.widget.addTextChangedListener
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -10,24 +11,38 @@ import com.example.android_intern.databinding.ActivityMainBinding
 import com.google.gson.Gson
 
 
-private const val TAG = "Tag"
+private const val SETTING = "filterSetting"
+private const val TEXT = "filterText"
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private var sharedPreference: SharedPreferences? = null
     private val gson = Gson()
     private val phoneAdapter = PhoneAdapter()
+    private var loadFilter = ""
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        Log.d(TAG, "onCreate")
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        sharedPreference = getSharedPreferences(SETTING, Context.MODE_PRIVATE)
         initRecyclerView()
-        firstFilling()
+        loadFilter = sharedPreference?.getString(TEXT, "").toString()
+        firstFilling(loadFilter)
         setListeners()
     }
 
-    private fun firstFilling() {
-        phoneAdapter.submitList(getInitNumbers())
+    private fun firstFilling(loadFilter: String) {
+        if (loadFilter.isEmpty()) {
+            phoneAdapter.submitList(getInitNumbers())
+        } else {
+            phoneAdapter.submitList(getInitNumbers().filter {
+                "${it.name} ${it.phone} ${it.type}".contains(
+                    loadFilter
+                )
+            })
+            binding.etInputFilter.setText(loadFilter)
+        }
+
     }
 
     private fun setListeners() {
@@ -35,7 +50,14 @@ class MainActivity : AppCompatActivity() {
             val filteredList =
                 getInitNumbers().filter { "${it.name} ${it.phone} ${it.type}".contains(binding.etInputFilter.text) }
             phoneAdapter.submitList(filteredList)
+            saveData()
         }
+    }
+
+    private fun saveData() {
+        val sharedEditor = sharedPreference?.edit()
+        sharedEditor?.putString(TEXT, binding.etInputFilter.text.toString())
+        sharedEditor?.apply()
     }
 
     private fun getInitNumbers() =
@@ -47,4 +69,13 @@ class MainActivity : AppCompatActivity() {
         binding.recyclerView.adapter = phoneAdapter
     }
 
+    override fun onDestroy() {
+        super.onDestroy()
+        saveData()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        saveData()
+    }
 }
