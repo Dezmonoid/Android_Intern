229d3a3d95d6eb555fcd760206764b9b93be482f
Dezmonoid <Dezmoonoid@mail.ru>
2023-09-30 13:21:55 +0300

    Внесение изменений в задание 24

--- app/src/main/java/com/example/android_intern/MainActivity.kt
+++ app/src/main/java/com/example/android_intern/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.android_intern
 
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.android_intern.databinding.ActivityMainBinding
@@ -15,6 +16,7 @@ const val BASE_URL = "https://api.openweathermap.org/data/2.5/"
 const val APP_ID = "eeba719e0ea1ed0d70d6ea433307695e"
 const val UNITS = "metric"
 const val CITY_ID = "622034"
+const val TAG = "Error"
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
@@ -30,14 +32,14 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun loadWeather() {
-        apiService.getCurrentForecastData(CITY_ID, APP_ID, UNITS)
+        apiService.getCurrentForecastData(city_id = CITY_ID, app_id = APP_ID, units = UNITS)
             .enqueue(object : Callback<ForecastResponse> {
                 override fun onResponse(
                     call: Call<ForecastResponse>,
                     response: Response<ForecastResponse>
                 ) {
                     if (response.isSuccessful) {
-                        val forecastGetList = response.body()!!.list
+                        val forecastGetList = response.body()?.list
                         runOnUiThread {
                             adapter.submitList(forecastGetList)
                         }
@@ -45,7 +47,7 @@ class MainActivity : AppCompatActivity() {
                 }
 
                 override fun onFailure(call: Call<ForecastResponse>, t: Throwable) {
-                    t.printStackTrace()
+                    Log.e(TAG,t.message,t)
                 }
             })
     }
