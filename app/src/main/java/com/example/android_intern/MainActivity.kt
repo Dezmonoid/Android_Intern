edd2b0f164b8ceae5c289e563fb60859ec562769
Dezmonoid <Dezmoonoid@mail.ru>
2023-12-05 13:44:44 +0300

    Внесение изменений в задание 27

--- app/src/main/java/com/example/android_intern/MainActivity.kt
+++ app/src/main/java/com/example/android_intern/MainActivity.kt
@@ -12,11 +12,11 @@ import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
-const val BASE_URL = "https://api.openweathermap.org/data/2.5/"
-const val APP_ID = "eeba719e0ea1ed0d70d6ea433307695e"
-const val UNITS = "metric"
-const val CITY_ID = "622034"
-const val TAG = "Debug"
+private const val BASE_URL = "https://api.openweathermap.org/data/2.5/"
+private const val APP_ID = "eeba719e0ea1ed0d70d6ea433307695e"
+private const val UNITS = "metric"
+private const val CITY_ID = "622034"
+private const val TAG = "Debug"
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
@@ -27,43 +27,35 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         initWeatherRecyclerView()
+        initApiService()
         loadWeather()
     }
 
     private fun loadWeather() {
         if (WeatherStore.get().isEmpty()) {
-            initApiService()
-            firstLoad()
-        } else {
-            loadingSaveWeather()
-        }
-    }
-
-    private fun loadingSaveWeather() {
-        adapter.submitList(WeatherStore.get())
-    }
-
-    private fun firstLoad() {
-        apiService.getCurrentForecastData(cityId = CITY_ID, appId = APP_ID, units = UNITS)
-            .enqueue(object : Callback<ForecastResponse> {
-                override fun onResponse(
-                    call: Call<ForecastResponse>,
-                    response: Response<ForecastResponse>
-                ) {
-                    if (response.isSuccessful) {
-                        Log.d(TAG, binding.root.context.getString(R.string.connected))
-                        val forecastList = response.body()?.list.orEmpty()
-                        WeatherStore.set(forecastList)
-                        adapter.submitList(forecastList)
-                    } else {
-                        Log.d(TAG, binding.root.context.getString(R.string.error_connected))
+            apiService.getCurrentForecastData(cityId = CITY_ID, appId = APP_ID, units = UNITS)
+                .enqueue(object : Callback<ForecastResponse> {
+                    override fun onResponse(
+                        call: Call<ForecastResponse>,
+                        response: Response<ForecastResponse>
+                    ) {
+                        if (response.isSuccessful) {
+                            Log.d(TAG, binding.root.context.getString(R.string.connected))
+                            val forecastList = response.body()?.list.orEmpty()
+                            WeatherStore.set(forecastList)
+                            adapter.submitList(forecastList)
+                        } else {
+                            Log.d(TAG, binding.root.context.getString(R.string.error_connected))
+                        }
                     }
-                }
 
-                override fun onFailure(call: Call<ForecastResponse>, t: Throwable) {
-                    Log.e(TAG, t.message, t)
-                }
-            })
+                    override fun onFailure(call: Call<ForecastResponse>, t: Throwable) {
+                        Log.e(TAG, t.message, t)
+                    }
+                })
+        } else {
+            adapter.submitList(WeatherStore.get())
+        }
     }
 
     private fun initApiService() {
