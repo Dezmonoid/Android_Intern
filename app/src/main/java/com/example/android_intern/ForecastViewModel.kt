656ce6dc2fb8b355c638733b1469772818ae4cce
Dezmonoid <Dezmoonoid@mail.ru>
2023-12-16 17:21:53 +0300

    Выполнение задания 38

--- app/src/main/java/com/example/android_intern/ForecastViewModel.kt
+++ app/src/main/java/com/example/android_intern/ForecastViewModel.kt
@@ -1,17 +1,14 @@
 package com.example.android_intern
 
-import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.launch
 
 private const val APP_ID = "eeba719e0ea1ed0d70d6ea433307695e"
 private const val UNITS = "metric"
 private const val CITY_ID = "622034"
-private const val TAG = "Debug"
 
 class ForecastViewModel : ViewModel() {
     private val _liveData = MutableLiveData<List<ForecastResponse.Sky>>()
@@ -23,19 +20,13 @@ class ForecastViewModel : ViewModel() {
     }
 
     private fun loadWeather() {
-        App.weatherApi.getCurrentForecastData(cityId = CITY_ID, appId = APP_ID, units = UNITS)
-            .enqueue(object : Callback<ForecastResponse> {
-                override fun onResponse(
-                    call: Call<ForecastResponse>,
-                    response: Response<ForecastResponse>
-                ) {
-                    val forecasts = response.body()?.list.orEmpty()
-                    _liveData.value = forecasts
-                }
-
-                override fun onFailure(call: Call<ForecastResponse>, t: Throwable) {
-                    Log.e(TAG, t.message, t)
-                }
-            })
+        viewModelScope.launch {
+            val forecasts = App.weatherApi.getCurrentForecastData(
+                cityId = CITY_ID,
+                appId = APP_ID,
+                units = UNITS
+            ).execute().body()?.list.orEmpty()
+            _liveData.value = forecasts
+        }
     }
 }
